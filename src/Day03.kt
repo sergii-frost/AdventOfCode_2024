7e0ce061d3973b3c1366e4fb92a631e91bcce073
Sergii <sergii.nezdolii@bontouch.com>
2024-12-05 16:59:36 +0100

    Day3

---
+++ src/Day03.kt
@@ -0,0 +1,52 @@
+class Day03(val path: String) {
+    private val program: String
+
+    init {
+        val input = InputReader().readInput(path)
+        //All the input must be read as one string
+        program = input.joinToString()
+    }
+
+    fun mulItOverTask1(): Long {
+        return Instruction(program).mul()
+    }
+
+    fun mulItOverTask2(): Long {
+        return Instruction(program).mulCorrupted()
+    }
+}
+
+private data class Instruction(val input: String) {
+    private val mulRegex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+    private val mulDoNotDoRegex = Regex("don't\\(\\).*?(?:do\\(\\)|$)")
+    private val numberRegex = Regex("\\d+")
+
+    fun mul(): Long {
+        return mul(input)
+    }
+
+    fun mulCorrupted(): Long {
+        val allMatches = mulDoNotDoRegex.findAll(input)
+        println("Number of matches: ${allMatches.count()}")
+        val cleanInput = mulDoNotDoRegex.replace(input, "")
+        return mul(cleanInput)
+    }
+
+    private fun mul(input: String): Long {
+        return mulRegex.findAll(input).map {
+            var mulResult: Long = 1
+            numberRegex.findAll(it.value).forEach { num -> mulResult *= num.value.toLong() }
+            mulResult
+        }.sum()
+    }
+}
+
+private fun main() {
+    val day03Sample = Day03("day03_input_sample.txt")
+    val day03Sample1 = Day03("day03_input_task2_sample.txt")
+    val day03Task = Day03("day03_input.txt")
+    println("Sample Task1: ${day03Sample.mulItOverTask1()}")
+    println("Real Task1: ${day03Task.mulItOverTask1()}")
+    println("Sample Task2: ${day03Sample1.mulItOverTask2()}")
+    println("Real Task2: ${day03Task.mulItOverTask2()}")
+}
